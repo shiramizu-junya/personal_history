a2f80ebbe7fa91fe22edcdd292392b318500da63
shiramizu_junya <jyunya.pg.7231@gmail.com>
2021-08-26 20:55:43 +0900

    [Add] jQueryライブラリを追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,7 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery");
+require("jquery")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -14,3 +14,11 @@ require("jquery");
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// ハンバーガーメニューのアニメーション
+$(function () {
+  $(".navbar-burger").on("click", function () {
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+  });
+});
