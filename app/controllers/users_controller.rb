17d8a41709334ded664e7feb7acc6a36237837c2
shiramizu_junya <jyunya.pg.7231@gmail.com>
2021-08-28 15:12:17 +0900

    [Add] ユーザー登録のロジックを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
+
+  #  ユーザー新規作成画面
   def new
+    @user = User.new
   end
 
+  # ユーザー新規作成機能
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      # ユーザー登録後の自動ログイン
+      auto_login(@user)
+      redirect_to tracks_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
