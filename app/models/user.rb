4f1feda30e5c8420f06d2ef1b09d2f47be2db8d6
shiramizu_junya <jyunya.pg.7231@gmail.com>
2021-08-28 22:24:56 +0900

    [Fix] エラー対応のためバリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
 
   validates :name, presence: true, length: { maximum: 255 }
-  validates :email, uniqueness: true, presence: true
+  validates :email, uniqueness: { case_sensitive: true }, presence: true
   validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
