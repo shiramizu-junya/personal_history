33f74707608efcba524cf86b66c7261be918234e
shiramizu_junya <jyunya.pg.7231@gmail.com>
2021-08-28 09:40:41 +0900

    [Add] ユーザー登録のバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :email, uniqueness: true, presence: true
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 end
