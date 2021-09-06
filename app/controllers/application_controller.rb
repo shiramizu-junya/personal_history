97c8f62a5bb8d9c2bfccdca96337e18d6935c784
shiramizu_junya <jyunya.pg.7231@gmail.com>
2021-09-06 09:41:54 +0900

    [Add] not_authenticatedメソッドを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
-  add_flash_types :success, :info, :warning, :danger
+  add_flash_types :success, :danger
   before_action :require_login
+
+  private
+
+  def not_authenticated
+    flash[:warning] = t("defaults.message.require_login")
+    redirect_to login_path
+  end
 end
