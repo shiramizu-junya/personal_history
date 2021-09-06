b35df6dfdb825fc05c3a9384eec27fb9ed6a0319
shiramizu_junya <jyunya.pg.7231@gmail.com>
2021-09-06 09:38:07 +0900

    [Add] ユーザー登録・ログインの画面と機能を追加

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,11 +5,10 @@ class UserSessionsController < ApplicationController
 
   def create
     @user = login(params[:email], params[:password], params[:remember])
-
     if @user
-      redirect_back_or_to tracks_path, success: "ログインに成功しました" #t('.success')
+      redirect_back_or_to tracks_path, success: t(".success")
     else
-      flash.now[:danger] = "ログインに失敗しました" #t('.fail')
+      flash.now[:danger] = t(".fail")
       render :new
     end
   end
@@ -19,6 +18,6 @@ class UserSessionsController < ApplicationController
     remember_me!
     forget_me!
     logout
-    redirect_to root_url, success: "ログアウトに失敗しました" #t('.success')
+    redirect_to root_url, success: t(".success")
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,19 @@
 class UsersController < ApplicationController
   skip_before_action :require_login, only: [:new, :create]
 
-  #  ユーザー新規作成画面
   def new
     @user = User.new
   end
 
-  # ユーザー新規作成機能
   def create
     @user = User.new(user_params)
 
     if @user.save
       # ユーザー登録後の自動ログイン
       auto_login(@user)
-      redirect_to tracks_path, success: "ユーザー登録に成功しました" #t('.success')
+      redirect_to tracks_path, success: t(".success")
     else
-      flash.now[:danger] = "ユーザー登録に失敗しました" #t('.fail')
+      flash.now[:danger] = t(".fail")
       render :new
     end
   end
