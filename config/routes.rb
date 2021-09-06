6ad8fd9b38f52fd89fa8b6425833ea1b66e70ee9
shiramizu_junya <jyunya.pg.7231@gmail.com>
2021-09-06 15:36:06 +0900

    [Fix] ダブルクオートに修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  root 'static_pages#top'
+  root "static_pages#top"
   # ユーザー登録関係
   resources :users, only: %i[new create]
   # ログイン/ログアウト関係
-  get 'login', to: 'user_sessions#new'
-  post 'login', to: 'user_sessions#create'
-  delete 'logout', to: 'user_sessions#destroy'
+  get "login", to: "user_sessions#new"
+  post "login", to: "user_sessions#create"
+  delete "logout", to: "user_sessions#destroy"
   # 軌跡関係
   resources :tracks, only: %i[index]
 end
