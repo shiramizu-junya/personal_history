3e99de78172194b7d619c2e1f64028679fcbb210
shiramizu_junya <jyunya.pg.7231@gmail.com>
2021-09-06 10:12:02 +0900

    [Add] headerとFontAwsomeを読み込むコードを追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
+require("../src/header")
+import "@fortawesome/fontawesome-free/js/all";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -14,11 +16,3 @@ require("jquery")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-// ハンバーガーメニューのアニメーション
-$(function () {
-  $(".navbar-burger").on("click", function () {
-    $(".navbar-burger").toggleClass("is-active");
-    $(".navbar-menu").toggleClass("is-active");
-  });
-});
