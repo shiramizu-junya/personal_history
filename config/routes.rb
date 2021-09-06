00eedac3526f964c6cde9d1f66c06a4210e0c083
shiramizu_junya <jyunya.pg.7231@gmail.com>
2021-09-06 10:12:42 +0900

    [Fix] コメントを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
-  # ユーザー登録
+  # ユーザー登録関係
   resources :users, only: %i[new create]
-  # ログイン/ログアウト
+  # ログイン/ログアウト関係
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+  # 軌跡関係
   resources :tracks
 end
