14e70a28d06519859eddf11c3bafb3b5eb46dff3
shiramizu_junya <jyunya.pg.7231@gmail.com>
2021-09-07 16:05:37 +0900

    [Add] ユーザー登録、ログイン、ログアウトのSystemSpecを作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    name { "tanaka_ken" }
+    name { "user" }
     sequence(:email) { |n| "user_#{n}@example.com" }
     password { "password" }
     password_confirmation { "password" }
