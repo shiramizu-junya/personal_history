b86b8d60148b61629863a78764733c7512f39519
shiramizu_junya <jyunya.pg.7231@gmail.com>
2021-09-07 10:43:27 +0900

    [Add] UserモデルのModelSpecを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    name { "tanaka_ken" }
+    sequence(:email) { |n| "user_#{n}@example.com" }
+    password { "password" }
+    password_confirmation { "password" }
   end
 end
