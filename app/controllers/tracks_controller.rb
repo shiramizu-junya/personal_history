6b847d5c74250c4d93d8297af44a22fa1d992f4e
shiramizu_junya <jyunya.pg.7231@gmail.com>
2021-09-09 21:42:30 +0900

    [Add] tracks_controller.rbにアクションを追加

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -2,4 +2,16 @@ class TracksController < ApplicationController
   skip_before_action :require_login, only: [:index]
 
   def index; end
+
+  def show; end
+
+  def new; end
+
+  def edit; end
+
+  def create; end
+
+  def update; end
+
+  def destroy; end
 end
