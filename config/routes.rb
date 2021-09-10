05d7fb2f5cafdfb7f36aa75c0787da0525dba997
shiramizu_junya <jyunya.pg.7231@gmail.com>
2021-09-10 16:06:30 +0900

    [Add] プロフィール編集ページと更新機能のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get "login", to: "user_sessions#new"
   post "login", to: "user_sessions#create"
   delete "logout", to: "user_sessions#destroy"
+  # プロフィール関係
+  resource :profile, only: %i[edit update]
   # 軌跡関係
   resources :tracks
 end
