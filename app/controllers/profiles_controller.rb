f40566b2af83f48470414ac03a6cf3a923efccb6
shiramizu_junya <jyunya.pg.7231@gmail.com>
2021-09-12 10:49:13 +0900

    [Add] プロフィール編集画面を追加

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+  before_action :set_user, only: %i[edit update]
+
   def show
   end
 
@@ -10,7 +12,9 @@ class ProfilesController < ApplicationController
   private
 
   def set_user
-    # @user = current_userを使うと、updateアクションに失敗した時にedit.html.erbの_header.html.erbのユーザー名を表示するとこに不具合が出る
+    # @user = current_userを使うと、updateアクションに失敗した時に_header.html.erbのユーザー名を表示するとこに不具合が出る
     @user = User.find(current_user.id)
   end
+
+  def user_params; end
 end
