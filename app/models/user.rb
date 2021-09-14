4e88b7f8fc1b390ff7f6f2ddf504b70512ffeff1
shiramizu_junya <jyunya.pg.7231@gmail.com>
2021-09-14 22:18:42 +0900

    [Fix] 不要なコードの削除とエラーメッセージの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,10 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 10 }
   validates :email, uniqueness: { case_sensitive: true }, presence: true
   validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
-  validates :gender, inclusion: { in: %w(men women), message: "性別を選択してください" }
+  validates :gender, inclusion: { in: %w(men women), message: "を選択してください" }
   validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
   enum gender: { men: 0, women: 1 }
-
-  # 画像名が重複しないよう変更する
-  def unique_image_name(user)
-    user.avatar.recreate_versions! if user.avatar?
-  end
 end
