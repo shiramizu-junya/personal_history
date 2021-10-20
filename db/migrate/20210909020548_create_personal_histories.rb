c34940a7c6d2cd658e553ace330809fd514e31aa
Shiramizu_Jyunya <jyunya.pg.7231@gmail.com>
2021-10-20 21:58:55 +0900

    [Fix] 公開・非公開設定のカラム名と列挙子がRubyの予約後と被るので修正

--- db/migrate/20210909020548_create_personal_histories.rb
+++ db/migrate/20210909020548_create_personal_histories.rb
@@ -1,7 +1,7 @@
 class CreatePersonalHistories < ActiveRecord::Migration[6.0]
   def change
     create_table :personal_histories do |t|
-      t.integer :published, null: false, default: 0
+      t.integer :status, null: false, default: 0
       t.string :title
       t.references :user, null: false, foreign_key: true
 
