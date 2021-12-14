fe0c6a1bd60f517e77fa3eaa839d6b612e851668
Shiramizu_Jyunya <jyunya.pg.7231@gmail.com>
2021-12-14 20:59:39 +0900

    [Fix] seedファイルにuuidカラム用のコードを追加

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -2,6 +2,7 @@ puts "users ..."
 
 150.times do |n|
   User.create!(
+    uuid: SecureRandom.uuid,
     name: "user_#{ n + 1 }",
     email: "sample_#{ n + 1 }@example.com",
     birthday: Faker::Date.between(from: "1923-04-01", to: "2007-04-01"),
