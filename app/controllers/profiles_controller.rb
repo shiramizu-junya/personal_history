4e860f870a9d7351ef87e08ab8967694d7ee6fd2
Shiramizu_Jyunya <jyunya.pg.7231@gmail.com>
2021-12-31 17:45:22 +0900

    [Add] マイページの作成

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,9 @@
 class ProfilesController < ApplicationController
-  before_action :set_user, only: %i[edit update]
+  before_action :set_user, only: %i[show edit update]
 
-  def show; end
+  def show
+    @my_history = @user.my_history
+  end
 
   def edit; end
 
