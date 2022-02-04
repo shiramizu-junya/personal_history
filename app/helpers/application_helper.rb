75306ea033abd0ff04a6dff0794a5e2989097d87
Shiramizu_Jyunya <jyunya.pg.7231@gmail.com>
2022-02-04 13:04:18 +0900

    [Fix] OGPとTwitter共有リンクの変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,18 +13,17 @@ module ApplicationHelper
 
   def default_meta_tags
     {
-      site: "つづる自分史 | 人生を振り返る旅に出よう",
-      title: "つづる自分史 | 人生を振り返る旅に出よう",
+      site: "つづる自分史",
+      title: "人生を振り返る旅に出よう",
       reverse: true,
       charset: "utf-8",
       separator: "|",
-      description: "人生を振り返る旅に出よう",
-      keywords: "自分史",
+      description: "つづる自分史は人生を振り返ることができるアプリです。自分の人生を振り返ってみませんか？",
+      keywords: "つづる自分史, 自分史, 自分史アプリ, 人生",
       canonical: request.original_url,
-      noindex: ! Rails.env.production?,
       icon: [
         { href: image_url("favicon/favicon.ico") },
-        # { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+        { href: image_url("favicon/ogp_apple_touch_icon.png"), rel: "apple-touch-icon", sizes: "180x180", type: "image/jpg" }
       ],
       og: {
         site_name: :site,
@@ -32,13 +31,13 @@ module ApplicationHelper
         description: :description,
         type: "website",
         url: request.original_url,
-        image: image_url("ogp/ogp.png"),
+        image: image_url("ogp/ogp_twitter_card.png"),
         locale: "ja_JP",
       },
       twitter: {
         card: "summary_large_image",
         site: "@webpg_jun",
-        image: image_url("ogp/ogp.png")
+        image: image_url("ogp/ogp_twitter_card.png")
       }
     }
   end
