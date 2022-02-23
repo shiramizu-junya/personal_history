f6a3832f5d5324b3f287ed7f823c55c6823f6ec0
Shiramizu_Jyunya <jyunya.pg.7231@gmail.com>
2022-02-23 09:53:12 +0900

    [Fix] 出来事の年齢はマイナスを弾くようにバリデーションを変更

--- app/validators/event_age_validator.rb
+++ app/validators/event_age_validator.rb
@@ -3,6 +3,10 @@ class EventAgeValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return unless value
     age = User.current_user.calc_age
-    record.errors.add(:age, "は過去しか入力できません") if value > age
+    if value > age
+      record.errors.add(:age, "は過去しか入力できません")
+    elsif value < 0
+      record.errors.add(:age, "は０以上で入力してください")
+    end
   end
 end
