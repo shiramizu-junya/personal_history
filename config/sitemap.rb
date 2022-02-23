7bcba2c346f4c30c88fcbbf0ad9fda3bbd8436e9
Shiramizu_Jyunya <jyunya.pg.7231@gmail.com>
2022-02-23 13:42:08 +0900

    [Fix] サイトマップのパスを変更

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -3,11 +3,12 @@ SitemapGenerator::Sitemap.default_host = "https://www.tsuzuru-jibunshi.com/"
 # pick a place safe to write the files
 SitemapGenerator::Sitemap.public_path = "tmp/"
 # inform the map cross-linking where to find the other maps
-SitemapGenerator::Sitemap.sitemaps_host = "http://s3-ap-northeast-1.amazonaws.com/#{Rails.application.credentials.aws[:s3_bucket]}/"
+SitemapGenerator::Sitemap.sitemaps_host = "https://s3-ap-northeast-1.amazonaws.com/#{Rails.application.credentials.aws[:s3_bucket]}/"
 # pick a namespace within your bucket to organize your maps
 SitemapGenerator::Sitemap.sitemaps_path = "sitemaps/"
 # store on S3 using Fog
-SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(fog_provider: "AWS",
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(
+                                          fog_provider: "AWS",
                                           aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
                                           aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
                                           fog_directory: Rails.application.credentials.aws[:s3_bucket],
