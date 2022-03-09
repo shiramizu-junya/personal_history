838bf31818f024227a7855cfeb49a95a0b114c7f
Shiramizu_Jyunya <jyunya.pg.7231@gmail.com>
2022-03-09 10:46:24 +0900

    [Add] マイページのテストを追加

--- spec/system/my_histories/my_history_show_spec.rb
+++ spec/system/my_histories/my_history_show_spec.rb
@@ -40,5 +40,82 @@ RSpec.describe "MyHistory#show", type: :system do
         end
       end
     end
+
+    context "ログイン後" do
+      let(:user) { create(:user, birthday: nil, gender: nil) }
+      before { login_as(user) }
+      let(:my_history) do
+        ->(title_flag = false, my_history_flag = false) { my_history_as(title_flag, my_history_flag) }
+      end
+
+      context "正常系" do
+        it "コメントができること" do
+          my_history.call(true, true)
+          visit(my_history_path(user.my_history.uuid))
+          fill_in("js-new-comment-body", with: "コメントしました")
+          click_button("投稿")
+          expect(page).to have_content("コメントしました")
+        end
+
+        it "いいねができること" do
+          my_history.call(true, true)
+          visit(my_history_path(user.my_history.uuid))
+          click_link("js-like-button-#{user.my_history.id}")
+          expect(page).to have_css(".unlike-btn")
+        end
+
+        it "コメントが編集できること" do
+          my_history.call(true, true)
+          visit(my_history_path(user.my_history.uuid))
+          fill_in("js-new-comment-body", with: "コメントしました")
+          click_button("投稿")
+          find("#js-comment-edit-icon").click
+          within ".dropdown-content" do
+            page.first(".js-edit-comment-button").click
+          end
+          fill_in("js-textarea-comment-#{user.comments[0].id}", with: "コメント")
+          click_button("更新")
+          expect(page).to have_content("コメント")
+        end
+
+        it "コメントが削除できること" do
+          my_history.call(true, true)
+          visit(my_history_path(user.my_history.uuid))
+          fill_in("js-new-comment-body", with: "コメントしました")
+          click_button("投稿")
+          find("#js-comment-edit-icon").click
+          click_link("削除")
+          page.driver.browser.switch_to.alert.text.should == "コメントを削除しますか？"
+          page.driver.browser.switch_to.alert.accept
+          expect(page).to have_no_content("コメントしました")
+        end
+
+        it "ダウンロードボタンが表示されていること" do
+          my_history.call(true, true)
+          visit(my_history_path(user.my_history.uuid))
+          expect(page).to have_button("ダウンロード")
+        end
+
+        it "編集ボタンをクリックすると、編集画面に遷移すること" do
+          my_history.call(true, true)
+          visit(my_history_path(user.my_history.uuid))
+          within ".show-page-edit-link" do
+            click_link("編集")
+          end
+          expect(current_path).to eq(edit_my_history_path(user.my_history.uuid))
+        end
+
+      end
+
+      context "異常系" do
+        it "コメントを500文字より多く入力した場合、エラーになること" do
+          my_history.call(true, true)
+          visit(my_history_path(user.my_history.uuid))
+          fill_in("js-new-comment-body", with: "あ" * 501)
+          click_button("投稿")
+          expect(page).to have_content("コメントは500文字以内で入力してください")
+        end
+      end
+    end
   end
 end
